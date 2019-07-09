4d9da1aeff08b4c3bbffdb791d1294638e400766
sushan <756715212@qq.com>
2019-07-09 15:04:09 +0800

    百科上传文件

--- testcase/pc/wiki/uploadFile.py
+++ testcase/pc/wiki/uploadFile.py
@@ -2,10 +2,15 @@
 # -*- coding:utf-8 -*-
 
 import unittest
+
+from bs4 import BeautifulSoup
 from selenium import webdriver
 import time
 import testcase.tool.isElement as isElement
 from selenium.webdriver import ActionChains
+from PIL import Image
+import pytesseract
+import os
 
 import testcase.pc.login as login
 
@@ -35,6 +40,53 @@ class UploadFile(unittest.TestCase):
         ActionChains(driver).move_to_element(move_element).perform()
         driver.find_element_by_link_text("上传文件").click()
         time.sleep(10)
+        driver.save_screenshot('D:/SensitiveWords/image/screen.png')
+        imgelement = driver.find_element_by_xpath('//*[@id="upload"]/table/tbody/tr[4]/td[2]/img')  # 定位验证码
+        location = imgelement.location  # 获取验证码x,y轴坐标
+        size = imgelement.size  # 获取验证码的长宽
+        rangle = (int(location['x']), int(location['y']), int(location['x'] + size['width']),
+                  int(location['y'] + size['height']))  # 写成我们需要截取的位置坐标
+        i = Image.open("D:/SensitiveWords/image/screen.png")  # 打开截图
+        frame4 = i.crop(rangle)  # 使用Image的crop函数，从截图中再次截取我们需要的区域
+        frame4.save('D:/SensitiveWords/image/imgCode.png')  # 保存我们接下来的验证码图片 进行打码
+        codeimg = Image.open('D:/SensitiveWords/image/imgCode.png')
+        # 把彩色图像转化为灰度图像。RBG转化到HSI彩色空间，采用I分量
+        imgry = codeimg.convert('L')
+        # 图片转为字符串
+        code = pytesseract.image_to_string(imgry)
+        time.sleep(5)
+        print('code:', code)
+        # 点击【选择文件】
+        driver.find_element_by_id('wpUploadFile').click()
+        time.sleep(10)
+        os.system("C:\\Users\\ce\\Desktop\\upfile.exe")
+        time.sleep(10)
+        # 目标文件名
+        driver.find_element_by_id('wpDestFile').clear()
+        driver.find_element_by_id('wpDestFile').click()
+        driver.find_element_by_id('wpDestFile').send_keys('上传违规图片.jpg')
+        # 文件描述
+        driver.find_element_by_id('wpUploadDescription').click()
+        driver.find_element_by_id('wpUploadDescription').send_keys('自动化测试上传违规图片是否拦截')
+        time.sleep(5)
+        # 填写验证码
+        driver.find_element_by_name('wpCode').click()
+        driver.find_element_by_name('wpCode').send_keys(code)
+        time.sleep(3)
+        driver.find_element_by_name('wpUpload').click()
+        time.sleep(10)
+        # 上传文件后返回值
+        html = driver.page_source
+        soup = BeautifulSoup(html, 'lxml')
+        res = soup.select_one('#content > div.firstHeading-wrap > h1')
+        print("res", res)
+        if res == '图片内容不合法':
+            print("拦截图片内容不合法")
+        else:
+            print("图片内容不合法未拦截")
+        # 删除验证码图片
+        os.remove('D:/SensitiveWords/image/screen.png')
+        os.remove('D:/SensitiveWords/image/imgCode.png')
 
 
     def tearDown(self):
